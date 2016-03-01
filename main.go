820b26c3a291e9f5a44628c53791e96898a42593
Marc Villacorta <marc.villacorta@gmail.com>
2016-03-01 18:29:09 +0100

    Switch to kingpin

--- main.go
+++ main.go
@@ -11,10 +11,12 @@ package main
 import (
 
 	// Standard library:
-	"flag"
 	"fmt"
 	"os"
 	"text/template"
+
+	// Community:
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 type Udata struct {
@@ -27,63 +29,22 @@ type Udata struct {
 
 var (
 
-	// udata
-	fleettags string
-	ns1apikey string
-	role      string
-	domain    string
-	hostname  string
+	// Top level:
+	app   = kingpin.New("coreseed", "CoreSeed is used to deploy CoreOS clusters.")
+	debug = app.Flag("debug", "Enable debug mode.").Bool()
 
-	// start
-	pktApiKey       string
-	pktProjectName  string
-	pktDevicePrefix string
-	pktPlan         string
-	pktFacility     string
-	pktOS           string
+	// udata: nested command
+	cmdUdata    = app.Command("udata", "Generate CoreOS cloud-config user-data.")
+	flHostName  = cmdUdata.Flag("hostname", "Short host name as in (hostname -s).").Required().String()
+	flDomain    = cmdUdata.Flag("domain", "Domain name as in (hostname -d).").Required().String()
+	flRole      = cmdUdata.Flag("role", "Choose one of [ master | slave | edge]").Required().String()
+	flNs1Apikey = cmdUdata.Flag("ns1apikey", "NS1 API key.").Required().String()
+	flFleetTags = cmdUdata.Flag("fleettags", "Comma separated list of tags.").String()
+
+	// run: nested command
+	cmdRun      = app.Command("run", "Starts a CoreOS instance.")
+	flPktApiKey = cmdRun.Arg("pktApiKey", "Packet API key.").Required().String()
 )
-
-//-----------------------------------------------------------------------------
-// func init() is called after all the variable declarations in the Package
-// have evaluated their initializers, and those are evaluated only after all
-// the imported packages have been initialized:
-//-----------------------------------------------------------------------------
-
-func init() {
-
-	switch os.Args[1] {
-	case "udata":
-		flag.StringVar(&fleettags, "fleet-tags", "", "Comma separated list of tags.")
-		flag.StringVar(&ns1apikey, "ns1-api-key", "", "NS1. API key.")
-		flag.StringVar(&role, "role", "slave", "Choose one of [ master | slave | edge]")
-		flag.StringVar(&domain, "domain", "cell-1.dc-1.demo.lan", "Domain name as in 'hostname -d'")
-		flag.StringVar(&hostname, "hostname", "core-1", "Short host name as in 'hostname -s'")
-		flag.Usage = usage
-		flag.Parse()
-	case "start":
-		flag.StringVar(&pktApiKey, "packet-api-key", "", "Packet private API key.")
-		flag.StringVar(&pktProjectName, "packet-project-name", "", "Packet project name.")
-		flag.StringVar(&pktDevicePrefix, "packet-device-prefix", "", "Packet device prefix.")
-		flag.StringVar(&pktPlan, "packet-plan", "", "Packet plan.")
-		flag.StringVar(&pktFacility, "packet-facility", "", "Packet facility.")
-		flag.StringVar(&pktOS, "packet-os", "", "Packet OS.")
-		flag.Usage = usage
-		flag.Parse()
-	default:
-		usage()
-	}
-	usage()
-}
-
-//----------------------------------------------------------------------------
-// func usage() reports the correct commandline usage:
-//----------------------------------------------------------------------------
-
-func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s [ udata | start ] [arg...]\n", os.Args[0])
-	flag.PrintDefaults()
-	os.Exit(2)
-}
 
 //----------------------------------------------------------------------------
 //
@@ -91,11 +52,18 @@ func usage() {
 
 func main() {
 
-	udata := Udata{
-		Hostname: hostname,
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case cmdUdata.FullCommand():
+		println("CMD: udata")
+	case cmdRun.FullCommand():
+		println("CMD: start")
 	}
 
-	switch role {
+	udata := Udata{
+		Hostname: *flHostName,
+	}
+
+	switch *flRole {
 	case "master":
 		t := template.New("udata")
 		t, err := t.Parse(templ_master)
@@ -111,8 +79,6 @@ func main() {
 		t, err := t.Parse(templ_edge)
 		err = t.Execute(os.Stdout, udata)
 		checkError(err)
-	default:
-		usage()
 	}
 }
 
