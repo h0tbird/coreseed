722e9241bb3893a6344aad16e0ada776135ea3c9
Marc Villacorta <marc.villacorta@gmail.com>
2016-03-22 11:26:36 +0100

    Fill edge-role user-data

--- edge.go
+++ edge.go
@@ -8,5 +8,158 @@ package main
 // CoreOS edge user data:
 //---------------------------------------------------------------------------
 
-const templ_edge = `EDGE: Host name is edge-{{.HostId}}
+const templ_edge = `#cloud-config
+
+hostname: "edge-{{.HostId}}.{{.Domain}}"
+
+write_files:
+
+ - path: "/etc/hosts"
+   content: |
+    127.0.0.1 localhost
+    $private_ipv4 edge-{{.HostId}}.{{.Domain}} edge-{{.HostId}}
+    $private_ipv4 edge-{{.HostId}}.int.{{.Domain}} edge-{{.HostId}}.int
+    $public_ipv4 edge-{{.HostId}}.ext.{{.Domain}} edge-{{.HostId}}.ext
+
+ - path: "/etc/resolv.conf"
+   content: |
+    search {{.Domain}}
+    nameserver 8.8.8.8
+
+ {{if .CaCert }}- path: "/etc/docker/certs.d/internal-registry-sys.marathon:5000/ca.crt"
+   content: |
+    {{.CaCert}}{{end}}
+
+ - path: "/etc/systemd/system/docker.service.d/50-docker-opts.conf"
+   content: |
+    [Service]
+    Environment='DOCKER_OPTS=--registry-mirror=http://external-registry-sys.marathon:5000'
+
+ - path: "/home/core/.bashrc"
+   owner: "core:core"
+   content: |
+    [[ $- != *i* ]] && return
+    alias ls='ls -hF --color=auto --group-directories-first'
+    alias l='ls -l'
+    alias ll='ls -la'
+    alias grep='grep --color=auto'
+    alias dim='docker images'
+    alias dps='docker ps'
+    alias drm='docker rm -v $(docker ps -qaf status=exited)'
+    alias drmi='docker rmi $(docker images -qf dangling=true)'
+    alias drmv='docker volume rm $(docker volume ls -qf dangling=true)'
+
+ - path: "/etc/ssh/sshd_config"
+   permissions: "0600"
+   content: |
+    UsePrivilegeSeparation sandbox
+    Subsystem sftp internal-sftp
+    ClientAliveInterval 180
+    UseDNS no
+    PermitRootLogin no
+    AllowUsers core
+    PasswordAuthentication no
+    ChallengeResponseAuthentication no
+
+ - path: "/opt/bin/ns1dns"
+   permissions: "0755"
+   content: |
+    #!/bin/bash
+
+    readonly HOST="$(hostname -s)"
+    readonly DOMAIN="$(hostname -d)"
+    readonly APIURL='https://api.nsone.net/v1'
+    readonly APIKEY='{{.Ns1ApiKey}}'
+    declare -A IP=(['ext']='$public_ipv4' ['int']='$private_ipv4')
+
+    for i in ext int; do
+
+      curl -sX GET -H "X-NSONE-Key: ${APIKEY}" \
+      ${APIURL}/zones/${i}.${DOMAIN}/${HOST}.${i}.${DOMAIN}/A | \
+      grep -q 'record not found' && METHOD='PUT' || METHOD='POST'
+
+      curl -sX ${METHOD} -H "X-NSONE-Key: ${APIKEY}" \
+      ${APIURL}/zones/${i}.${DOMAIN}/${HOST}.${i}.${DOMAIN}/A -d "{
+        \"zone\":\"${i}.${DOMAIN}\",
+        \"domain\":\"${HOST}.${i}.${DOMAIN}\",
+        \"type\":\"A\",
+        \"answers\":[{\"answer\":[\"${IP[${i}]}\"]}]}"
+
+    done
+
+ - path: "/opt/bin/etchost"
+   permissions: "0755"
+   content: |
+    #!/bin/bash
+
+    PUSH=$(cat /etc/hosts | grep $(hostname -s)) \
+    && etcdctl set /hosts/$(hostname) "${PUSH}"
+
+    PULL='127.0.0.1 localhost'$'\n'
+    for i in $(etcdctl ls /hosts 2>/dev/null | sort); do
+      PULL+=$(etcdctl get ${i})$'\n'
+    done
+
+    echo "${PULL}" | grep -q $(hostname -s) && echo "${PULL}" > /etc/hosts
+
+ - path: "/opt/bin/loopssh"
+   permissions: "0755"
+   content: |
+    #!/bin/bash
+    A=$(fleetctl list-machines -fields=ip -no-legend)
+    for i in $A; do ssh -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no $i -C "$*"; done
+
+coreos:
+
+ units:
+
+  - name: "etcd2.service"
+    command: "start"
+
+  - name: "fleet.service"
+    command: "start"
+
+  - name: "ns1dns.service"
+    command: "start"
+    content: |
+     [Unit]
+     Description=Publish DNS records to nsone
+     Before=etcd2.service
+
+     [Service]
+     Type=oneshot
+     ExecStart=/opt/bin/ns1dns
+
+  - name: "etchost.service"
+    content: |
+     [Unit]
+     Description=Stores IP and hostname in etcd
+     Requires=etcd2.service
+     After=etcd2.service
+
+     [Service]
+     Type=oneshot
+     ExecStart=/opt/bin/etchost
+
+  - name: "etchost.timer"
+    command: "start"
+    content: |
+     [Unit]
+     Description=Run etchost.service every 5 minutes
+
+     [Timer]
+     OnBootSec=2min
+     OnUnitActiveSec=5min
+
+ fleet:
+  public-ip: "$private_ipv4"
+  metadata: "role=edge,id={{.HostId}}"
+
+ etcd2:
+  name: "edge-{{.HostId}}"
+  initial-cluster: "master-1=http://master-1:2380,master-2=http://master-2:2380,master-3=http://master-3:2380"
+  advertise-client-urls: "http://$private_ipv4:2379"
+  listen-client-urls: "http://127.0.0.1:2379,http://$private_ipv4:2379"
+  proxy: on
 `
