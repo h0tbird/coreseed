7722ed7238399d72e46a9f22a9a3bcd3dbb001d8
Marc Villacorta <marc.villacorta@gmail.com>
2016-07-01 09:58:02 +0200

    Garbage collector removes dangling local volumes

--- udata/worker.go
+++ udata/worker.go
@@ -248,6 +248,7 @@ coreos:
        docker ps -q --no-trunc | sort -u > containers.running; \
        docker rm $$(comm -23 containers.all containers.running) 2>/dev/null; \
        docker rmi $$(docker images -qf dangling=true) 2>/dev/null; \
+       docker volume rm $(docker volume ls -f dangling=true | awk "/^local/ {print $2}") 2>/dev/null; \
        etcdctl set /docker/images/$$(hostname) "$$(docker ps --format "{{"{{"}}.Image{{"}}"}}" | sort -u)"; \
        for i in $$(etcdctl ls /docker/images); do etcdctl get $$i; done | sort -u > images.running; \
        docker images | awk "{print \$$1\\":\\"\$$2}" | sed 1d | sort -u > images.local; \
