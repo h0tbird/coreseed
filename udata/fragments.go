d2ca03401891d2721de4a099235c83abd3dc59ac
Marc Villacorta <marc.villacorta@gmail.com>
2016-11-22 10:49:37 +0100

    Use quay.io/kato/mesos:v1.1.0-1

--- udata/fragments.go
+++ udata/fragments.go
@@ -885,7 +885,7 @@ coreos:
      TimeoutStartSec=0
      KillMode=mixed
      EnvironmentFile=/etc/kato.env
-     Environment=IMG=quay.io/kato/mesos:latest
+     Environment=IMG=quay.io/kato/mesos:v1.1.0-1
      ExecStartPre=/opt/bin/zk-alive ${KATO_QUORUM_COUNT}
      ExecStartPre=/usr/bin/rkt fetch ${IMG}
      ExecStartPre=/usr/bin/rkt run \
@@ -1493,7 +1493,7 @@ coreos:
      TimeoutStartSec=0
      KillMode=mixed
      EnvironmentFile=/etc/kato.env
-     Environment=IMG=quay.io/kato/mesos:latest
+     Environment=IMG=quay.io/kato/mesos:v1.1.0-1
      ExecStartPre=/opt/bin/zk-alive ${KATO_QUORUM_COUNT}
      ExecStartPre=/usr/bin/rkt fetch ${IMG}
      ExecStartPre=/usr/bin/rkt run \
