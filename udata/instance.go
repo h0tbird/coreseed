318347d8a80e34f20e1824f859d4fd6f9e64b2e2
Marc Villacorta <marc.villacorta@gmail.com>
2016-12-23 15:47:53 +0100

    Working on instance.go (WiP)

--- udata/instance.go
+++ udata/instance.go
@@ -22,6 +22,14 @@ type service struct {
 }
 
 //-----------------------------------------------------------------------------
+// func: serviceLink
+//-----------------------------------------------------------------------------
+
+func serviceLink(s *[]service, n string) *service {
+	return nil
+}
+
+//-----------------------------------------------------------------------------
 // func: loadServices
 //-----------------------------------------------------------------------------
 
@@ -30,15 +38,21 @@ func (d *Data) loadServices() {
 	//----------------------------------
 
 	d.services = append(d.services, service{
-		name:     "foo",
-		tcpPorts: []int{1, 2},
+		name:     "etcd2",
+		tcpPorts: []int{2379, 2380},
 	})
 
 	//----------------------------------
 
 	d.services = append(d.services, service{
-		name:     "bar",
-		tcpPorts: []int{3, 4},
+		name: "docker",
+	})
+
+	//----------------------------------
+
+	d.services = append(d.services, service{
+		name:     "zookeeper",
+		tcpPorts: []int{2888, 3888, 2181},
 	})
 }
 
@@ -53,8 +67,17 @@ func (d *Data) loadRoles() {
 	d.roles = append(d.roles, role{
 		name: "quorum",
 		baseServices: []*service{
-			&d.services[0],
-			&d.services[1],
+			serviceLink(&d.services, "etcd2"),
+			serviceLink(&d.services, "docker"),
+			serviceLink(&d.services, "zookeeper"),
+			serviceLink(&d.services, "rexray"),
+			serviceLink(&d.services, "etchost.timer"),
+		},
+		promServices: []*service{
+			serviceLink(&d.services, "rkt-api"),
+			serviceLink(&d.services, "cadvisor"),
+			serviceLink(&d.services, "node-exporter"),
+			serviceLink(&d.services, "zookeeper-exporter"),
 		},
 	})
 
@@ -63,8 +86,61 @@ func (d *Data) loadRoles() {
 	d.roles = append(d.roles, role{
 		name: "master",
 		baseServices: []*service{
-			&d.services[0],
-			&d.services[1],
+			serviceLink(&d.services, "etcd2"),
+			serviceLink(&d.services, d.NetworkBackend),
+			serviceLink(&d.services, "docker"),
+			serviceLink(&d.services, "rexray"),
+			serviceLink(&d.services, "mesos-master"),
+			serviceLink(&d.services, "mesos-dns"),
+			serviceLink(&d.services, "marathon"),
+			serviceLink(&d.services, "etchost.timer"),
+		},
+		promServices: []*service{
+			serviceLink(&d.services, "rkt-api"),
+			serviceLink(&d.services, "cadvisor"),
+			serviceLink(&d.services, "confd"),
+			serviceLink(&d.services, "alertmanager"),
+			serviceLink(&d.services, "prometheus"),
+			serviceLink(&d.services, "node-exporter"),
+			serviceLink(&d.services, "mesos-master-exporter"),
+		},
+	})
+
+	//----------------------------------
+
+	d.roles = append(d.roles, role{
+		name: "worker",
+		baseServices: []*service{
+			serviceLink(&d.services, "etcd2"),
+			serviceLink(&d.services, d.NetworkBackend),
+			serviceLink(&d.services, "docker"),
+			serviceLink(&d.services, "rexray"),
+			serviceLink(&d.services, "go-dnsmasq"),
+			serviceLink(&d.services, "mesos-agent"),
+			serviceLink(&d.services, "marathon-lb"),
+			serviceLink(&d.services, "etchost.timer"),
+		},
+		promServices: []*service{
+			serviceLink(&d.services, "rkt-api"),
+			serviceLink(&d.services, "cadvisor"),
+			serviceLink(&d.services, "mesos-agent-exporter"),
+			serviceLink(&d.services, "node-exporter"),
+			serviceLink(&d.services, "haproxy-exporter"),
+		},
+	})
+
+	//----------------------------------
+
+	d.roles = append(d.roles, role{
+		name: "border",
+		baseServices: []*service{
+			serviceLink(&d.services, "etcd2"),
+			serviceLink(&d.services, d.NetworkBackend),
+			serviceLink(&d.services, "docker"),
+			serviceLink(&d.services, "rexray"),
+			serviceLink(&d.services, "mongodb"),
+			serviceLink(&d.services, "pritunl"),
+			serviceLink(&d.services, "etchost.timer"),
 		},
 	})
 }
