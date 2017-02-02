5b15413230d89970ff6dc771f0be4c912fbb3566
Marc Villacorta <marc.villacorta@gmail.com>
2017-02-03 00:06:34 +0100

    Update to Calico 2.0.2

--- udata/fragments.go
+++ udata/fragments.go
@@ -1066,10 +1066,10 @@ coreos:
      TimeoutStartSec=0
      KillMode=mixed
      EnvironmentFile=/etc/kato.env
-     Environment=CNI_URL=https://github.com/projectcalico/calico-cni/releases/download/v1.5.1
-     Environment=CALICOCTL_URL=https://github.com/projectcalico/calico-containers/releases/download/v1.0.0
+     Environment=CNI_URL=https://github.com/projectcalico/calico-cni/releases/download/v1.5.6
+     Environment=CALICOCTL_URL=https://github.com/projectcalico/calico-containers/releases/download/v1.0.2
      Environment=CNI_PLUGINS=/var/lib/cni-plugins
-     Environment=IMG=quay.io/calico/node:v1.0.0
+     Environment=IMG=quay.io/calico/node:v1.0.2
      ExecStartPre=/usr/sbin/sysctl -w net.netfilter.nf_conntrack_max=1000000
      ExecStartPre=/usr/bin/sh -c "[ -d /var/run/calico ] || mkdir /var/run/calico"
      ExecStartPre=-/bin/bash -c " \
