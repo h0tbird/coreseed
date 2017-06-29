22c27bd342b4d0a416076f84498ed30faf13bf8b
Marc Villacorta <marc.villacorta@gmail.com>
2017-06-29 15:37:28 +0200

    Update Marathon to 1.4.5

--- pkg/udata/udata_fragments.go
+++ pkg/udata/udata_fragments.go
@@ -1240,7 +1240,7 @@ coreos:
      KillMode=mixed
      LimitNOFILE=8192
      EnvironmentFile=/etc/kato.env
-     Environment=IMG=quay.io/kato/marathon:v1.4.3-1
+     Environment=IMG=quay.io/kato/marathon:v1.4.5-1
      ExecStartPre=/opt/bin/zk-alive ${KATO_QUORUM_COUNT}
      ExecStartPre=/usr/bin/rkt fetch ${IMG}
      ExecStart=/usr/bin/rkt run \
